90e64b71af81919067f82b0208b42fd38865aa46
Omar Gomez <omar.gomez@gmail.com>
2021-01-15 11:27:24 -0500

    10 UNit testing

---
+++ 10 UNIT TESTION/10 UNIT TESTION/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  10 UNIT TESTION
+//
+//  Created by Omar Eduardo Gomez Padilla on 14/01/21.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("AutoBrake") {
+  // Unit test here
+    REQUIRE(1 == 0);
+}
+
+TEST_CASE("AutoBrake2") {
+  // Unit test here
+    REQUIRE(1 == 2);
+}
+
